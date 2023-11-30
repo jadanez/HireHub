337f717b9cc0ea890c78f65003df39895e0bcc36
jadanez <122333839+jadanez@users.noreply.github.com>
2023-11-29 19:09:28 -0500

    added dummy files on empty folders

---
+++ HireHub/Employers/Models/dummymodel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HireHub.Employers.Models
+{
+    internal class dummymodel
+    {
+    }
+}
